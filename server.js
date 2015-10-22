e895db21b0dc858320403a0b93ccb0af03890e2c
Ubuntu <ubuntu@ip-172-31-17-254.us-west-1.compute.internal>
2015-10-22 22:28:06 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,33 @@
+//Lets require/import the HTTP module
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+//Lets define a port we want to listen to
+const PORT=80;
+     
+//Create a server
+var server = http.createServer(function(request, response) {
+    var file      = path.basename(request.url) || 'index.html';
+    console.log(request.url);  
+    fs.exists(file, function(exists) {
+        if(exists) {
+            fs.readFile(file, function(err, data) {
+                if(!err){
+                    response.end(data);
+                } else {
+                    console.dir(err);
+                }
+            });
+        } else {
+            response.writeHead(404, {'Content-Type': 'text/html'});
+            response.end("404 page not found");
+        }
+    });
+});
+    
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
