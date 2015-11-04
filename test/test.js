17838476632748e768b2ced56ce268301a6d2b5b
Kanra90 <setton77@gmail.com>
2015-11-04 03:44:54 +0000

    cleaned, added test w/ Nyan Reporter

--- test/test.js
+++ test/test.js
@@ -1,9 +1,7 @@
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
+describe ('addition', function (){
+it('should add 1+1 correctly',function(){
+  var num = 1 + 1;
+  assert.equal(num,2);
   });
 });
