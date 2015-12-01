0f9bc98bef56d6b692bac01ce562445966f4f5dd
zettaslow <brandonsmith1994@hotmail.com>
2015-12-01 21:47:57 +0000

    Added new website templating method with janky SteamAPI access

--- buildHTML.js
+++ buildHTML.js
@@ -8,39 +8,76 @@ var dbConnection = mysql.createConnection({
   database : 'SteamGames'
   });
 
-dbConnection.connect();
+var appID1 = 4000;
+var appID2 = 322170;
+var appID3 = 105600;
 
+dbConnection.connect();
+var discountSQL = "SELECT * FROM GamesList WHERE SalePrice != Price ORDER BY salePrice ASC LIMIT 20;";
+var sql1 = "SELECT DISTINCT game.Id, game.Name, game.Tags, game.Price, game.SalePrice, game.Image FROM GamesList as game WHERE game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',1) FROM GamesList WHERE Id = "+appID1+" LIMIT 1),'%') AND game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',-1) FROM GamesList WHERE Id = "+appID1+" LIMIT 1),'%' ) ORDER BY RAND() LIMIT 10;";
+var sql2 = "SELECT DISTINCT game.Id, game.Name, game.Tags, game.Price, game.SalePrice, game.Image FROM GamesList as game WHERE game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',1) FROM GamesList WHERE Id = "+appID2+" LIMIT 1),'%') AND game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',-1) FROM GamesList WHERE Id = "+appID2+" LIMIT 1),'%' ) ORDER BY RAND() LIMIT 10;";
+var sql3 = "SELECT DISTINCT game.Id, game.Name, game.Tags, game.Price, game.SalePrice, game.Image FROM GamesList as game WHERE game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',1) FROM GamesList WHERE Id = "+appID3+" LIMIT 1),'%') AND game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',-1) FROM GamesList WHERE Id = "+appID3+" LIMIT 1),'%' ) ORDER BY RAND() LIMIT 10;";
+
+var queryResults = {};
+var salesHTML;
+var gameQueue;
+var gamesList;
 fs.readFile("./templates/pContainer.html.handlebars", 'utf-8', function(err, data) {
   if(err) {
-    console.log("BOOOOOOOOOOM");
     console.log(err);
     process.exit(0);
   }
-  var queryResults;
-  var sql = 'SELECT * FROM `GamesList` WHERE (1-SalePrice/Price)*100>30 ORDER BY SalePrice ASC LIMIT 50;';
-  dbConnection.query(sql, function(error, results, fields) {
-    queryResults = { "gameItem" : results };
+  
+  dbConnection.query(discountSQL, function(error, results, fields) {
+    queryResults.gameItem = results;
     var template = handlebars.compile(data);
-    var pContainers = template(queryResults);
-    fs.readFile("./templates/s.html.handlebars", 'utf-8', function(err, data) {
-      if(err) {
-        console.log("BOOOOOOOOOOM");
-        console.log(err);
+    salesHTML = template(queryResults);
+    dbConnection.query(sql1, function(error, results1, fields1) {
+      if(error){
+        console.log(error);
         process.exit(0);
       }
-
-      var gamez = { "gameList" : pContainers };
-
-      var sTemplate = handlebars.compile(data);
-
-      fs.writeFile("s.html", sTemplate(gamez), 'utf-8', function(err) {
-        if(err){
-          console.log("BOOOOOOOM");
-          console.log(err);
+      gamesList = results1;
+      dbConnection.query(sql2, function(error, results2, fields2) {
+        if(error){
+          console.log(error);
           process.exit(0);
         }
-        console.log("Full s.html page generated!");
-        dbConnection.end();
+        gamesList.concat(results2);
+        dbConnection.query(sql3, function(error, results3, fields3) {
+          if(error){
+            console.log(error);
+            process.exit(0);
+          }
+          gamesList.concat(results3);
+          queryResults.gameQueueItem = gamesList;
+          fs.readFile("./templates/userQueue.html.handlebars", 'utf-8', function(err,data) {
+            if(err){
+              console.log(err);
+              process.exit(0);
+            }
+            var template = handlebars.compile(data);
+            gameQueue = template(queryResults);
+            fs.readFile("./templates/s.html.handlebars", 'utf-8', function(err,data) {
+              if(err){
+                console.log(err);
+                process.exit(0);
+              }
+              var template = handlebars.compile(data)
+              var pageContext = { "gameList" : salesHTML,
+                                  "gameQueue": gameQueue };
+
+              fs.writeFile("s.html", template(pageContext), 'utf-8', function(err,data){
+                if(err){
+                  console.log(err);
+                  process.exit();
+                }
+                console.log("s.html has been compiled!");
+                dbConnection.end();
+              });
+            });
+          });
+        });
       });
     });
   });
--- steamAPIFunctions.js
+++ steamAPIFunctions.js
@@ -1,8 +1,13 @@
+var async = require('async');
+var fs = require('fs');
+var handlebars = require('handlebars')
 var Steam = require('steam-webapi');
 var url = require("url");
 Steam.key = "9FBE22017BDAFE1A29251305717AB46B";
-var steamProfileURL = "http://steamcommunity.com/profiles/76561198085418340/";
-var vanityID ="Acorns";
+//var steamProfileURL = "http://steamcommunity.com/profiles/76561198085418340/";
+//var vanityID ="Acorns";
+var steamProfileURL = "http://steamcommunity.com/profiles/76561198030289343";
+var vanityID ="Akimbo Chicken Strips";
 var sql1,sql2,sql3;
 var mysql = require('mysql');
 
@@ -12,6 +17,18 @@ var dbConnection = mysql.createConnection({
   password : 'letmein',
   database : 'SteamGames'
 });
+
+
+var getGames = function(sql, callback){
+  dbConnection.query(sql, function(err, results, fields){
+    if(err){
+      console.log(err);
+      process.exit(0);
+    }
+    callback(results);
+  });
+};
+
 function parseSteamProfileURL(URL)
 {
 	var pathname = url.parse(steamProfileURL).pathname;
@@ -19,7 +36,7 @@ function parseSteamProfileURL(URL)
 	return arr[2];
 }
 
-var ID = "Acorns";
+var ID = "LostSoulforLegit";
 //var ID = parseSteamProfileURL(steamProfileURL);
 
 
@@ -87,11 +104,16 @@ function ownedGames(err, data)
 			console.log("Name: " + data.games[ndx].name);
       console.log("AppID: " + data.games[ndx].appid);
       console.log("Playtime: " + data.games[ndx].playtime_forever + "\n");
-      
-      sql1 = "SELECT DISTINCT game.Id, game.Name, game.Tags FROM GamesList as game WHERE game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',1) FROM GamesList WHERE Id = " + appID1 + "LIMIT 1),'%') AND game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',-1) FROM GamesList WHERE Id = " + appID1 + " LIMIT 1),'%') LIMIT 10;";
-      sql2 = "SELECT DISTINCT game.Id, game.Name, game.Tags FROM GamesList as game WHERE game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',1) FROM GamesList WHERE Id = " + appID2 + "LIMIT 1),'%') AND game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',-1) FROM GamesList WHERE Id = " + appID2 + " LIMIT 1),'%') LIMIT 10;";
-      sql3 = "SELECT DISTINCT game.Id, game.Name, game.Tags FROM GamesList as game WHERE game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',1) FROM GamesList WHERE Id = " + appID3 + "LIMIT 1),'%') AND game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',-1) FROM GamesList WHERE Id = " + appID3 + " LIMIT 1),'%') LIMIT 10;";
     }
-	});
-}
+    console.log(appID1 + "," + appID2 + "," + appID3);
+      var discountSQL = 'SELECT * FROM `GamesList` WHERE (1-SalePrice/Price)*100>30 ORDER BY SalePrice ASC LIMIT 50;';
+      sql1 = "SELECT DISTINCT game.Id, game.Name, game.Tags FROM GamesList as game WHERE game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',1) FROM GamesList WHERE Id = " + appID1 + " LIMIT 1),'%') AND game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',-1) FROM GamesList WHERE Id = " + appID1 + " LIMIT 1),'%') LIMIT 10;" 
+      sql2 = "SELECT DISTINCT game.Id, game.Name, game.Tags FROM GamesList as game WHERE game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',1) FROM GamesList WHERE Id = " + appID2 + " LIMIT 1),'%') AND game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',-1) FROM GamesList WHERE Id = " + appID2 + " LIMIT 1),'%') LIMIT 10;" 
+      sql3 = "SELECT DISTINCT game.Id, game.Name, game.Tags FROM GamesList as game WHERE game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',1) FROM GamesList WHERE Id = " + appID3 + " LIMIT 1),'%') AND game.tags LIKE CONCAT('%',(SELECT DISTINCT SUBSTRING_INDEX(tags,',',-1) FROM GamesList WHERE Id = " + appID3 + " LIMIT 1),'%') LIMIT 10;"
 
+  console.log(sql1);
+  console.log(sql2);
+  console.log(sql3);
+
+  });
+}
